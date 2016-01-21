7d7bfb4a30cb9a24ff067ca15d039a771082aada
Ezequiel Urrizaga <ezequiel.urrizaga@globant.com>
2016-01-21 16:06:09 -0300

    cambios

--- js/app.js
+++ js/app.js
@@ -1,10 +1,4 @@
 "use strict";
 $(window).ready(function(){
-	$("#add_comment").bind("click", addComment);
-
-	function addComment(){
-		var new_text = $("#new_comment").val();
-		$("#comment_area").append("<p>" + new_text + "</p>");
-	}
-
+	Comments.init();
 });
--- js/comments.js
+++ js/comments.js
@@ -2,16 +2,17 @@ var Comments = {};
 (function ($) {
 
   Comments.init = function () {
-  	$("#add_comment").bind("click", addComment);
-	$("#comment_area > *").css("background-color","blue");
+  	//insertar componentes
+  	$("#comment_widget").load("widget/comments.html", function(){
+		$("#add_comment").bind("click", this.addComment);	
+  	});
+  	
   }
-
 
   Comments.addComment = function () {
   	var new_text = $("#new_comment").val();
 	$("#comment_area").append("<p class='comment'>" + new_text + "</p>");
 	$("#new_comment").val("");
+	alert('Comment Added');
   }
-
-
 })(jQuery);
