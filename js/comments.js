e3df43306e870e94f71a3fd9c5f043e19ddbd2fb
Ezequiel Urrizaga <ezequiel.urrizaga@globant.com>
2016-01-22 10:55:45 -0300

    modificaciones visuales

--- js/comments.js
+++ js/comments.js
@@ -2,16 +2,19 @@ var Comments = {};
 (function ($) {
   Comments.init = function () {
   	//insertar componentes
-  	this.insertHTML($("comment-widget"));
+  	//this.insertHTML($("comment-widget"));
   	$("#add_comment").bind("click", this.addComment);	
   }
 
   Comments.addComment = function () {
   	var new_text = $("#new_comment").val();
-	$("#comment_area").append("<p class='comment'>" + new_text + "</p>");
+  	var new_name = $("#name").val();
+  	var date = new Date($.now());
+	$("#comment_area").append("<li class='comment' title='Posted by " + new_name + " on " + date + "'><strong>"+ new_name +" said: </strong>" + new_text + "</li>");
 	$("#new_comment").val("");
+	$("#new_name").val("");
   }
-
+/*
   Comments.insertHTML = function(element){
   	element.html('<div id="row1" class="row"></div><div id="row2" class="row"></div>');
   	$("#row1").append('<p>Comments:</p>')
@@ -20,9 +23,9 @@ var Comments = {};
   		.append('<div id= "new_comment_area" class="col-xs-4"></div>')
   		.append('<div class="col-xs-4"></div>');
   	$("#new_comment_area").append('<p>Add new Comment</p>')
-  		.append('<p>Enter your Name: <input type="text" value=""/></p>')
+  		.append('<p>Enter your Name: <input type="text" id="name" value=""/></p>')
   		.append('<p>Enter your Comment: <textarea id="new_comment"></textarea></p>')
   		.append('<p><button id="add_comment" class="btn btn-success">ADD COMMENT</button></p>');
-
   }
+  */
 })(jQuery);
